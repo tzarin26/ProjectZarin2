a84ca039ca94a9b4b18cb6a99f251d9618cfdaae
zarin <tasnimz@sheridancollege.ca>
2026-06-01 21:31:01 -0400

    Add new feature

---
+++ ProjectZarin2/src/main/java/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Zarin Tasnim, 2026
+ */
+public class Main {
+    
+     public static void main(String[] args) {
+     
+     System.out.println("Hello from feature branch");
+     }
+    
+}
